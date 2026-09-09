2d12e453b6ca6ff09813ed478b678c979b98df3b
souravcuh <souravcuh@gmail.com>
2026-09-09 07:57:50 +0530

    Add verified state temple photo galleries for Tirtha map hover panels

---
+++ assets/data/tirtha-state-gallery.js
@@ -0,0 +1,200 @@
+window.TIRTHA_STATE_GALLERIES = {
+  "Uttarakhand": [
+    {
+      "title": "Kedarnath",
+      "file": "Kedarnath shrine.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Kedarnath_shrine.jpg"
+    },
+    {
+      "title": "Badrinath",
+      "file": "Badrinath Temple.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Badrinath_Temple.jpg"
+    },
+    {
+      "title": "Gangotri",
+      "file": "Gangotri temple.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Gangotri_temple.jpg"
+    }
+  ],
+  "Uttar Pradesh": [
+    {
+      "title": "Kashi Vishwanath",
+      "file": "Kashi vishwanath temple varanasi.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Kashi_vishwanath_temple_varanasi.jpg"
+    },
+    {
+      "title": "Sri Ram Mandir · Ayodhya",
+      "file": "Ram Mandir Ayodhya.png",
+      "page": "https://commons.wikimedia.org/wiki/File:Ram_Mandir_Ayodhya.png"
+    },
+    {
+      "title": "Mathura",
+      "file": "Mathura Temple-Mathura-India0002.JPG",
+      "page": "https://commons.wikimedia.org/wiki/File:Mathura_Temple-Mathura-India0002.JPG"
+    }
+  ],
+  "Odisha": [
+    {
+      "title": "Jagannath · Puri",
+      "file": "Jagannath Temple in Puri district.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Jagannath_Temple_in_Puri_district.jpg"
+    },
+    {
+      "title": "Lingaraj · Bhubaneswar",
+      "file": "Lingaraj Temple, Bhubaneswar, Odisha.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Lingaraj_Temple,_Bhubaneswar,_Odisha.jpg"
+    },
+    {
+      "title": "Konark Sun Temple",
+      "file": "Konark Sun temple.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Konark_Sun_temple.jpg"
+    }
+  ],
+  "Tamil Nadu": [
+    {
+      "title": "Ramanathaswamy · Rameswaram",
+      "file": "Ramanathaswamy Temple, Rameswaram.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Ramanathaswamy_Temple,_Rameswaram.jpg"
+    },
+    {
+      "title": "Ranganathaswamy · Srirangam",
+      "file": "Ranganathaswamy Temple, Srirangam si0556.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Ranganathaswamy_Temple,_Srirangam_si0556.jpg"
+    },
+    {
+      "title": "Nataraja · Chidambaram",
+      "file": "Nataraja Temple, Chidambaram (51057092242).jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Nataraja_Temple,_Chidambaram_(51057092242).jpg"
+    }
+  ],
+  "Kerala": [
+    {
+      "title": "Sabarimala",
+      "file": "SabarimalaTemple.JPG",
+      "page": "https://commons.wikimedia.org/wiki/File:SabarimalaTemple.JPG"
+    },
+    {
+      "title": "Guruvayur",
+      "file": "Guruvayoor Temple, Kerala, India.JPG",
+      "page": "https://commons.wikimedia.org/wiki/File:Guruvayoor_Temple,_Kerala,_India.JPG"
+    },
+    {
+      "title": "Padmanabhaswamy",
+      "file": "Padmanabhaswamy Temple.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Padmanabhaswamy_Temple.jpg"
+    }
+  ],
+  "Gujarat": [
+    {
+      "title": "Dwarkadhish · Dwarka",
+      "file": "Dwarkadhish Temple, Dwarka, Gujarat.JPG",
+      "page": "https://commons.wikimedia.org/wiki/File:Dwarkadhish_Temple,_Dwarka,_Gujarat.JPG"
+    },
+    {
+      "title": "Somnath",
+      "file": "Somnath Temple, Somnath.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Somnath_Temple,_Somnath.jpg"
+    },
+    {
+      "title": "Ambaji",
+      "file": "Ambaji temple.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Ambaji_temple.jpg"
+    }
+  ],
+  "Andhra Pradesh": [
+    {
+      "title": "Sri Venkateswara · Tirumala",
+      "file": "Sri Venkateswara Swamy Temple in Tirumala in Chittoor district,Andhra Pradesh.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Sri_Venkateswara_Swamy_Temple_in_Tirumala_in_Chittoor_district,Andhra_Pradesh.jpg"
+    },
+    {
+      "title": "Mallikarjuna · Srisailam",
+      "file": "Entrance of Srisailam temple.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Entrance_of_Srisailam_temple.jpg"
+    },
+    {
+      "title": "Simhachalam",
+      "file": "Simhachalam Temple.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Simhachalam_Temple.jpg"
+    }
+  ],
+  "Maharashtra": [
+    {
+      "title": "Trimbakeshwar",
+      "file": "Trimbakeshwar Temple.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Trimbakeshwar_Temple.jpg"
+    },
+    {
+      "title": "Pandharpur",
+      "file": "Pandharpur Vithoba temple.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Pandharpur_Vithoba_temple.jpg"
+    },
+    {
+      "title": "Bhimashankar",
+      "file": "Bhimashankar temple, Maharashtra.JPG",
+      "page": "https://commons.wikimedia.org/wiki/File:Bhimashankar_temple,_Maharashtra.JPG"
+    }
+  ],
+  "Madhya Pradesh": [
+    {
+      "title": "Mahakaleshwar · Ujjain",
+      "file": "Mahakaleshwar Temple, Ujjain.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Mahakaleshwar_Temple,_Ujjain.jpg"
+    },
+    {
+      "title": "Omkareshwar",
+      "file": "Omkareshwar Temple.png",
+      "page": "https://commons.wikimedia.org/wiki/File:Omkareshwar_Temple.png"
+    }
+  ],
+  "West Bengal": [
+    {
+      "title": "Kalighat Kali",
+      "file": "Kalighat Kali Temple.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Kalighat_Kali_Temple.jpg"
+    },
+    {
+      "title": "Dakshineswar Kali",
+      "file": "Dakshineshwar Kali Temple.png",
+      "page": "https://commons.wikimedia.org/wiki/File:Dakshineshwar_Kali_Temple.png"
+    },
+    {
+      "title": "Tarapith",
+      "file": "Tarapith temple.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Tarapith_temple.jpg"
+    }
+  ],
+  "Assam": [
+    {
+      "title": "Kamakhya",
+      "file": "Kamakhya Temple in Assam.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Kamakhya_Temple_in_Assam.jpg"
+    }
+  ],
+  "Karnataka": [
+    {
+      "title": "Sringeri Sharada",
+      "file": "Sringeri Sharadha temple.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Sringeri_Sharadha_temple.jpg"
+    },
+    {
+      "title": "Murudeshwar",
+      "file": "Murudeshwar temple, Karnataka, Lord Shiva.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Murudeshwar_temple,_Karnataka,_Lord_Shiva.jpg"
+    }
+  ],
+  "Rajasthan": [
+    {
+      "title": "Brahma Temple · Pushkar",
+      "file": "Pushkar Brahma Temple.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Pushkar_Brahma_Temple.jpg"
+    }
+  ],
+  "Bihar": [
+    {
+      "title": "Vishnupad · Gaya",
+      "file": "Vishnupad temple gaya bihar.jpg",
+      "page": "https://commons.wikimedia.org/wiki/File:Vishnupad_temple_gaya_bihar.jpg"
+    }
+  ]
+};
